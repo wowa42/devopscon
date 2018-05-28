125a330375d413ea3ecc86e4129b48977129936a
wowa42 <wolfgang@walznet.de>
2018-05-28 16:31:19 +0200

    changed woman

--- build-war/src/main/java/org/devopscon/maven/demo/HelloHandler.java
+++ build-war/src/main/java/org/devopscon/maven/demo/HelloHandler.java
@@ -3,7 +3,7 @@ package org.devopscon.maven.demo;
 public class HelloHandler {
 
 	public String sayHello() {
-		String hello = "Hi man? What do you think about this WAR file?";
+		String hello = "Hi woman? What do you think about this WAR file?";
 		return hello;
 	}
 
